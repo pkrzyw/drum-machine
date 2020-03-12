a66fa337db9d4546d1acf5d91268a62686f1c97f
fshiper <przemyslaw.krzywodajc@gmail.com>
2020-03-12 19:44:08 +0100

    changed to functional components

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,49 +1,43 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
-class DrumPad extends Component {
-  constructor(props) {
-    super(props);
+const DrumPad = props => {
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  });
 
-    this.playMusic = this.playMusic.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-  }
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress);
-  }
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyPress);
-  }
-
-  handleKeyPress(e) {
-    if (e.keyCode === this.props.keyCode) {
-      this.playMusic();
+  const handleKeyPress = e => {
+    if (e.keyCode === props.keyCode) {
+      playMusic();
     }
-  }
-  playMusic() {
-    const sound = document.getElementById(this.props.innerText);
+  };
+  const playMusic = () => {
+    const sound = document.getElementById(props.innerText);
     sound.currentTime = 0;
     sound.play();
-    this.props.changeDispl(this.props.source);
-  }
-  render() {
-    return (
-      <div className="col-4">
-        <button
-          className="drum-pad btn btn-block py-3 my-2 justify-content-center"
-          type="button"
-          id={this.props.padId}
-          onClick={this.playMusic}
-        >
-          {this.props.innerText}
-          <audio
-            preload="none"
-            className="clip"
-            id={this.props.innerText}
-            src={this.props.source}
-          />
-        </button>
-      </div>
-    );
-  }
-}
+    props.changeDispl(props.source);
+  };
+
+  return (
+    <div className="col-4">
+      <button
+        className="drum-pad btn btn-block py-3 my-2 justify-content-center"
+        type="button"
+        id={props.padId}
+        onClick={playMusic}
+      >
+        {props.innerText}
+        <audio
+          preload="none"
+          className="clip"
+          id={props.innerText}
+          src={props.source}
+        />
+      </button>
+    </div>
+  );
+};
+
 export default DrumPad;
